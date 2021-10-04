d190d26a9f0d40d08261b5b38edc75e00255576a
jfcross4 <jfcross4@gmail.com>
2021-10-04 14:52:25 -0400

    adding data and updating readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # sports_lines
+
+
+## Prediction Tracker
+https://www.thepredictiontracker.com/football.php
+
+## FPI Explanation
+https://www.espn.com/blog/statsinfo/post/_/id/122612/an-inside-look-at-college-fpi
---
+++ temp.R
@@ -0,0 +1,16 @@
+nfl <- read.csv("NFL_Lines_09-19.csv")
+vegas <- read.csv("VegasLines2000-2017.csv")
+
+library(dplyr)
+
+nfl %>% filter(season==2017, type=="Week 1", home.team=="NE")
+vegas %>% filter(season==2017, type=="Week 1", home.team=="New England")
+
+vegas <- vegas %>% mutate(actual.total = home.score + away.score)
+
+library(ggplot2)
+
+vegas %>% ggplot(aes(over.under, actual.total))+geom_point()+
+  geom_smooth(method="lm")
+
+
