84ebb0bce516ebd7910dd47dbe12e17f8c6a234c
jfcross4 <jfcross4@gmail.com>
2022-02-07 12:42:39 -0500

    improved random forest model

--- NFL Fast R.R
+++ NFL Fast R.R
@@ -1,38 +1,131 @@
-# install.packages("nflfastR")
-# 
-# if (!require("remotes")) install.packages("remotes")
-# remotes::install_github("nflverse/nflfastR")
-
-# install.packages("tidyverse", type = "binary")
-# install.packages("ggrepel", type = "binary")
-# install.packages("ggimage", type = "binary")
-# install.packages("nflfastR", type = "binary")
-
 library(tidyverse)
 library(ggrepel)
 library(ggimage)
 library(nflfastR)
 library(rpart)
 library(rpart.plot)
+library(randomForest)
 
-data <- load_pbp(2019)
+data <- load_pbp(2007:2021)
+dplyr::glimpse(data)
+hist(data$wp)
 
-#View(data)
+## add variables (spread, timeouts, ball in 2nd half, home team, time remaining in half, 
+## ratio of point differential)
 
+data_orig <- data %>% filter(two_point_attempt==0) %>%
+  filter(!(play_type %in% c("no_play", "kickoff", "extra_point")))
 
-data_slim <- data %>% 
+hist(data_orig$wp)
+
+data_slim <- data %>% filter(two_point_attempt==0) %>%
+    filter(!(play_type %in% c("no_play", "kickoff", "extra_point"))) %>%
   dplyr::select(away_score, home_score, score_differential, 
-  game_seconds_remaining, yardline_100, posteam_type, down, ydstogo)
+  game_seconds_remaining, yardline_100, posteam_type, down, ydstogo, wp)
 
 data_slim <- data_slim %>% 
-  mutate(win = 1*(home_score > away_score), 
-         diff = home_score - away_score)
+  mutate(win_home_team = 1*(home_score > away_score), 
+         end_game_diff = home_score - away_score,
+         win_pos_team = ifelse(posteam_type=="home", win_home_team, 1-win_home_team)
+         )
 
-# score differential is from possession teams point of view
+# score differential is from possession teams point of view (at the time of the play)
 # posteam_type says whether the team with possesion is the home or away team
 
-freddie <- rpart(win ~ score_differential+posteam_type, 
-                 data=data_slim, maxdepth=3, cp=0)
-prp(freddie)
+# max depth is how how many layers the tree has
 
-one_game <- data %>% mutate(game_id = "2019_01_ATL_MIN")
+# freddie <- rpart(win_pos_team ~ score_differential+
+#                    game_seconds_remaining + yardline_100, 
+#                  data=data_slim, maxdepth=8, cp=0)
+# prp(freddie)
+# data_slim$predictions <- predict(freddie, data_slim)
+# 
+# data_slim %>% ggplot(aes(as.factor(predictions, win_pos_team)))+
+#   geom_jitter(size=0.1)+geom_smooth(method="lm")
+# 
+#   data_slim %>% group_by(predictions) %>% 
+#     summarize(n=n(), mean(win_pos_team))
+  
+AAE <- function(x, y){ mean(abs(x-y))}
+RMSE <- function(x, y){ sqrt(mean((x-y)^2))}
+
+
+# data_slim %>% summarize(AAE(predictions, win_pos_team)) #0.319
+# 
+# # best we could have done
+# data_slim %>% summarize(mean(2*(predictions*(1-predictions)))) #0.319
+# 
+# # worst we could have (realistically) done
+# data_slim %>% summarize(AAE(win_pos_team, 0.5))  #0.5
+# 
+# 
+# data_slim %>% summarize(RMSE(predictions, win_pos_team)) #0.399
+# 
+# 
+
+
+# worst we could have (realistically) done
+# data_slim %>% summarize(RMSE(win_pos_team, 0.5))  #0.5
+# 
+# nrow(data_slim) #37142
+
+sample_rows <- sample(1:nrow(data_slim), 
+                      size=round(nrow(data_slim)/2), replace=FALSE)
+
+
+train = data_slim[sample_rows, ]
+test = data_slim[-sample_rows, ]
+
+# freddie <- rpart(win_pos_team ~ score_differential+
+#                    game_seconds_remaining + yardline_100, 
+#                  data=train, maxdepth=30, cp=0.001)
+# 
+# freddie <- rpart(win_pos_team ~ ., 
+#                  data=train, maxdepth=3, cp=0.001)
+# 
+# 
+# prp(freddie)
+
+# test$predictions <- predict(freddie, test)
+# 
+# test %>% summarize(AAE(predictions, win_pos_team)) #0.318
+# 
+# # best we could have done
+# test %>% summarize(mean(2*(predictions*(1-predictions)))) #0.316
+# 
+# # worst we could have (realistically) done
+# test %>% summarize(AAE(win_pos_team, 0.5))  #0.5
+
+
+# test %>% summarize(RMSE(predictions, win_pos_team)) #0.401
+# 
+# test %>% summarize(RMSE(0.5, win_pos_team)) #0.5
+# one_game <- data_slim %>% filter(game_id == "2019_01_ATL_MIN")
+# 
+
+
+
+
+t <- Sys.time()
+rf <- randomForest(win_pos_team~score_differential+
+                     game_seconds_remaining + yardline_100 + 
+                     down+ydstogo, data=train, mtry=3,
+                   ntree=30,
+                   na.action=na.omit, nodesize=100)
+Sys.time()-t
+
+hist(predict(rf, newdata = test))
+varImpPlot(rf,type=2)
+
+test$predictions = predict(rf, newdata = test)
+
+test <- test %>% filter(!is.na(predictions), !is.na(wp))
+
+RMSE(test$predictions, test$win_pos_team)
+RMSE(test$wp, test$win_pos_team)
+
+ggplot(test, aes(predictions, win_pos_team))+geom_point()+geom_smooth()
+
+
+library(xgboost)
+
